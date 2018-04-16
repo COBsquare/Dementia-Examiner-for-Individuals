0a7e96bbc3b511554efbaeebc9dc75ad24f691cd
onurdurukan <durukan87@hotmail.com>
2018-04-17 00:29:37 +0300

    Update ExportToPDF.java

--- src/UserInteractions/ExportToPDF.java
+++ src/UserInteractions/ExportToPDF.java
@@ -1,42 +1,5 @@
 package UserInteractions;
 
-/*import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
- 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-public class TableSamples {
-    public static final String DEST = "results/tables/simple_table2.pdf";
- 
-    public static void main(String[] args) throws IOException,
-            DocumentException {
-        File file = new File(DEST);
-        file.getParentFile().mkdirs();
-        new TableSamples().createPdf(DEST);
-    }
-    public void createPdf(String dest) throws IOException, DocumentException {
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream(dest));
-        document.open();
-        PdfPTable table = new PdfPTable(8);
-        PdfPCell cell = new PdfPCell(new Phrase("hi"));
-        cell.setRowspan(2);
-        table.addCell(cell);
-        for(int aw = 0; aw < 14; aw++){
-            table.addCell("hi");
-        }
-        document.add(table);
-        document.close();
-    }
- 
-}*/
-
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
